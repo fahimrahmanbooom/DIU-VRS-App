7c5fc34ce44bd18faadd4dfea16904608a50a234
Fahim Rahman <fahimrahman@MacBookPro.local>
2021-12-09 21:53:30 +0600

    initial

---
+++ DIU VRS/App/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  DIU VRS
+//
+//  Created by Fahim Rahman on 12/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DIU VRS/App/DIU_VRSApp.swift
@@ -0,0 +1,17 @@
+//
+//  DIU_VRSApp.swift
+//  DIU VRS
+//
+//  Created by Fahim Rahman on 12/9/21.
+//
+
+import SwiftUI
+
+@main
+struct DIU_VRSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
